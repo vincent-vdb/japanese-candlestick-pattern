8298a43247444c08e139db2bbace2d0dafee8808
Vincent Vandenbussche <vandenbussche.vincent@gmail.com>
2021-08-16 17:32:26 +0200

    adding trend to hammer to differentiate from hanging man

--- patterns/hammer.py
+++ patterns/hammer.py
@@ -4,31 +4,43 @@ from patterns.pattern import Pattern
 
 
 class Hammer(Pattern):
-    def __init__(self, data):
+    def __init__(self, data, trend_threshold: float = 0.03):
         """Constructor of Hammer class
 
         Parameters
         ----------
         data : pandas dataframe
             A pandas dataframe, expected to have at least the Open, High, Low, Close, Volume columns
+        trend_threshold : float
+            The relative threshold above which the trend is considered non neutral.
+            e.g. with the default value of 0.03, there has to be a relative trend of
+            at least -3% to be considered a Hammer pattern.
+            See Pattern documentation for relative trend computation details.
         """
         super().__init__(data)
+        self.trend = self.compute_relative_trend()
+        self.trend_threshold = trend_threshold
 
     def compute_pattern(self):
         """
-        Computes if a candlestick is a Hammer (or Hanging man, since no trend is computed)
+        Computes if a candlestick is a Hammer
 
         Definition is following from Steve Nison:
         - Real body is on upper part (no matter the color)
         - Lower shadow should be at least two times the real body
-        - No or really small upper shadow (actually quite same as condition 1)
+        - No or really small upper shadow, here computed as at most 50% of real body height
+        - In a downward/bearish trend
 
         Returns
         -------
         self.data : pandas.DataFrame
             the input dataframe, with a new column 'hammer' with bool
         """
-        self.data['hammer'] = np.logical_and(self.lower_shadow >= 2 * np.abs(self.real_body),
-                                             self.upper_shadow <= 0.05 * self.total_range)
+
+        hammer = np.all([self.lower_shadow >= 2 * np.abs(self.real_body),
+                         self.upper_shadow <= 0.5 * self.total_range,
+                         self.trend <= -self.trend_threshold],
+                        axis=0)
+        self.data['hammer'] = hammer
 
         return self.data
