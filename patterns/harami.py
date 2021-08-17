69aad62255071584e72756a444f8f640cc32952c
Vincent Vandenbussche <vandenbussche.vincent@gmail.com>
2021-08-17 14:35:52 +0200

    fix #2 adding pct change to Harami pattern

--- patterns/harami.py
+++ patterns/harami.py
@@ -4,7 +4,7 @@ from patterns.pattern import Pattern
 
 
 class Harami(Pattern):
-    def __init__(self, data, harami_threshold: float = 2.):
+    def __init__(self, data, harami_threshold: float = 2., percent_change_threshold: float = 0.03):
         """Constructor of Harami class
 
         Parameters
@@ -13,17 +13,23 @@ class Harami(Pattern):
             A pandas dataframe, expected to have at least the Open, High, Low, Close, Volume columns
         harami_threshold : float
             The minimum ratio between a previous long and current short candle to consider it a Harami
+        percent_change_threshold : float
+            The minimum percent change of the first candle in the Harami pattern.
+            e.g. with the default value of 0.03, the first candle has to have a 3% change
+            at least to be considered a Harami pattern.
         """
         super().__init__(data)
         self.harami_threshold = harami_threshold
+        self.percent_change = self.compute_percent_change()
+        self.percent_change_threshold = percent_change_threshold
 
     def compute_pattern(self):
         """
         Computes if a candlestick is a Harami.
         Conditions are the following from Steve Nison:
-        - two adjacent candles, the first with long real body, the second
+        - two adjacent candles, the first with long real body (a large percentage change), the second
         with short real body (e.g. at least 2 times shorter than the first one)
-        - the two candles do not necessarily opposite colors, but have to in crypto (since 24/7 and almost no gaps)
+        - the two candles do not necessarily have opposite colors, but have to in crypto (since 24/7 and almost no gaps)
 
         Harami strength is computed according to the following rule of Nison:
         - The smaller the second candle real body, the higher the strength (best case, cross harami with doji)
@@ -37,7 +43,8 @@ class Harami(Pattern):
                 positive when bullish engulfing, negative when bearing engulfing
         """
         harami_strength = self.real_body.shift() / self.real_body
-        harami = harami_strength < -self.harami_threshold
+        harami = np.logical_and(np.abs(harami_strength) >= self.harami_threshold,
+                                np.abs(self.percent_change.shift()) >= self.percent_change_threshold)
         self.data['harami'] = harami
         self.data['harami_strength'] = np.abs(harami_strength) * harami * np.sign(self.real_body)
 
