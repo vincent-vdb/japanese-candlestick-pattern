901e3b769dc7ca5ea018b4c9c1d0c2d18c4b62a7
Vincent Vandenbussche <vandenbussche.vincent@gmail.com>
2021-08-18 22:12:23 +0200

    fix #4: harami wrongly triggered

--- patterns/harami.py
+++ patterns/harami.py
@@ -13,6 +13,7 @@ class Harami(Pattern):
             A pandas dataframe, expected to have at least the Open, High, Low, Close, Volume columns
         harami_threshold : float
             The minimum ratio between a previous long and current short candle to consider it a Harami
+            A value of 2 means we expect the long candle's real body to be twice larger then second candle.
         percent_change_threshold : float
             The minimum percent change of the first candle in the Harami pattern.
             e.g. with the default value of 0.03, the first candle has to have a 3% change
@@ -43,8 +44,10 @@ class Harami(Pattern):
                 positive when bullish engulfing, negative when bearing engulfing
         """
         harami_strength = self.real_body.shift() / self.real_body
-        harami = np.logical_and(np.abs(harami_strength) >= self.harami_threshold,
-                                np.abs(self.percent_change.shift()) >= self.percent_change_threshold)
+        harami = np.all([np.abs(harami_strength) >= self.harami_threshold,
+                         harami_strength < 0,# to make sure the candles are in opposite directions
+                         np.abs(self.percent_change.shift()) >= self.percent_change_threshold],
+                        axis=0)
         self.data['harami'] = harami
         self.data['harami_strength'] = np.abs(harami_strength) * harami * np.sign(self.real_body)
 
