73656a2b56ace59f1ddcddf494087e7b180edc11
Vincent Vandenbussche <vandenbussche.vincent@gmail.com>
2021-08-19 16:21:04 +0200

    fixing #7 trend computation mistake

--- patterns/pattern.py
+++ patterns/pattern.py
@@ -74,6 +74,6 @@ class Pattern:
         # Compute the average value between Close and Open
         average = 0.5 * (self.data.Close + self.data.Open)
         # Compute the trend as the diff between those values
-        trend = (average - average.iloc[-trend_lookback]) / average
+        trend = (average - average.shift(trend_lookback)) / average
 
         return trend
